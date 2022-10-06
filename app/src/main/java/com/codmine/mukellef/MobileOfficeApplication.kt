c34597f5b706858cd773559bfb0ba6dae4e84a1c
hbemretasci <hbemretasci@gmail.com>
2022-10-06 14:47:37 +0300

    push notification implementation

--- app/src/main/java/com/codmine/mukellef/MobileOfficeApplication.kt
+++ app/src/main/java/com/codmine/mukellef/MobileOfficeApplication.kt
@@ -1,7 +1,21 @@
 package com.codmine.mukellef
 
 import android.app.Application
+import com.onesignal.OneSignal
 import dagger.hilt.android.HiltAndroidApp
 
+const val ONESIGNAL_APP_ID = "66d6306b-13cd-400c-9e9d-51e5cd0ae0b3"
+
 @HiltAndroidApp
-class MobileOfficeApplication: Application()
+class MobileOfficeApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+    }
+}
