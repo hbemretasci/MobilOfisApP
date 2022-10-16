0e518bf752955b0d30be85f6a15f7cac98aa8018
hbemretasci <hbemretasci@gmail.com>
2022-10-16 20:18:36 +0300

    push notification implementation

--- app/src/main/java/com/codmine/mukellef/MobileOfficeApplication.kt
+++ app/src/main/java/com/codmine/mukellef/MobileOfficeApplication.kt
@@ -11,9 +11,6 @@ class MobileOfficeApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Logging set to help debug issues, remove before releasing your app.
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
-
         // OneSignal Initialization
         OneSignal.initWithContext(this)
         OneSignal.setAppId(ONESIGNAL_APP_ID)
